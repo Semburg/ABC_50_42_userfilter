da215fc1fc9a21fa66e30b025f7da9085a6f7753
Simon Burg <semburg.sb@gmail.com>
2021-09-01 11:25:07 +0200

    done

---
+++ main.js
@@ -0,0 +1,53 @@
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+
+const listItems = []
+
+getData()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
+
+
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=15')
+
+    const { results } = await res.json()
+
+    result.innerHTML = ''
+
+    results.forEach(user => {
+
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+
+
+        // console.log(user)
+    });
+
+    // console.log(data)
+}
+
+function filterData(search) {
+
+    listItems.forEach(item => {
+        if (item.innerText.toLowerCase().includes(search.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+
+    // console.log(search)
+}
